04649a28198f7561559a638da5fbf57bceb54dfb
charlyfdz <carlos.afdzf@gmail.com>
2024-06-01 14:07:19 -0600

    actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath:"/corporativo-syf.com",
+    output: "export",
+    reactStrictMode: true
+};
 
 export default nextConfig;
